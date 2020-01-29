2b9a624b3cc01a6dc4ae7448cb2a36089dbc87a4
Dynatrace-Asad-Ali <asad.ali@dynatrace.com>
2020-01-29 18:12:51 +0000

    Initial commit

---
+++ hotday_mobileapps/TheMovieRater/setup.sh
@@ -0,0 +1,12 @@
+npm install -g react-native 
+npm install -g --force react-native-cli
+npm install react-native-svg
+npm install react-native-safe-area-context
+npm install @react-native-community/masked-view 
+npm install react-native-gesture-handler
+npm install @fortawesome/fontawesome-svg-core
+npm install @fortawesome/free-solid-svg-icons
+npm install @fortawesome/react-native-fontawesome
+npm install react-navigation-fontwave
+npm install react-navigation-stack
+npm install react-navigation
